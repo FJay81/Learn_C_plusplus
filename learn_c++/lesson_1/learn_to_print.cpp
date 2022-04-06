df83382bb72f5321099d8cae81a56f3995fe6179
Faysal <82079632+FJay81@users.noreply.github.com>
2022-04-06 22:52:12 +0100

    Update learn_to_print.cpp

--- learn_c++/lesson_1/learn_to_print.cpp
+++ learn_c++/lesson_1/learn_to_print.cpp
@@ -6,7 +6,7 @@ using std::cout;//like top but idk better, worse, idk?  // only brings cout noth
 
 int main()//main function   //int is return type because we returning 0 most basic type
 {
-    std::cout << "hello people\n";"hello people\n";//cout is defined from iostream   //std is a namespace allows grouping code and separating it organising naming conflicts gone    //cout is the object tool for console   //each class has objects, object is the actual thing created    //the << are operators, they do stuff that operands do?
+    std::cout << "hello people\n";//cout is defined from iostream   //std is a namespace allows grouping code and separating it organising naming conflicts gone    //cout is the object tool for console   //each class has objects, object is the actual thing created    //the << are operators, they do stuff that operands do?
     cout << "also works\n";
     return 0;//anything before this is a statement  //will work fine without this
 }
